b8cb9c21fc961b3d8ec43ae62f694dc4e1f261ca
s20017 <s20017@std.it-college.ac.jp>
2022-02-23 03:19:25 +0900

    first git

--- main.js
+++ main.js
@@ -13,16 +13,34 @@ app.use(layouts);
 app.get("/", (req, res) => {
     res.render("index")
 })
-
+//news
 app.get("/news", (req, res) =>{
     res.render("news")
 })
-
+//study
+app.get("/study", (req, res) =>{
+    res.render("study")
+})
+//foods
+app.get("/foods", (req, res) =>{
+    res.render("foods")
+})
+//hobby
+app.get("/hobby", (req, res) =>{
+    res.render("hobby")
+})
+//chat
+app.get("/chat", (req, res) =>{
+    res.render("chat")
+})
+//login
 app.get("/login", (req, res) =>{
     res.render("./login.ejs")
 })
 app.post("login", (req, res) =>{
 })
+
+//account create
 app.get("/create", (req, res) =>{
     res.render("./create.ejs")
 })
