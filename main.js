7ad91ea138eaebad2d9cac5d4376170f0252e808
s20017 <s20017@std.it-college.ac.jp>
2022-02-23 17:44:31 +0900

    first git

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const express = require("express"),
     layouts = require("express-ejs-layouts"),
     app = express();
 
-const routes = require("/routes/routes");
-app.use("/test", routes);
-
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: false}))
@@ -26,7 +23,7 @@ db.once("open", ()=>{
 app.use(layouts);
 app.get("/", (req, res) => {
     res.render("index")
-});
+})
 
 //news
 app.get("/news", (req, res) =>{
@@ -36,40 +33,38 @@ app.get("/news", (req, res) =>{
 //study
 app.get("/study", (req, res) =>{
     res.render("study")
-});
+})
 
 //foods
 app.get("/foods", (req, res) =>{
     res.render("foods")
-});
+})
 
 //hobby
 app.get("/hobby", (req, res) =>{
     res.render("hobby")
-});
+})
 
 //chat
 app.get("/chat", (req, res) =>{
     res.render("chat")
-});
+})
 
 //thread
 app.get("/thread", (req, res) =>{
     res.render("./thread")
 })
-app.post("/thread", (req, res) =>{
-
-});
 
 //login
 app.get("/login", (req, res) =>{
     res.render("./login")
-});
+})
 
 //account create
 app.get("/create", (req, res) =>{
     res.render("./create")
-});
+})
+
 
 app.listen(app.get("port"), () =>{
     console.log(`server http://localhost:${app.get("port")}`)
